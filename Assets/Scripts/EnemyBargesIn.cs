68e8cd82ef170040a1840836844957001a059e38
mwrig891 <152934086+mwrig891@users.noreply.github.com>
2026-03-20 16:10:03 -0400

    added door close and enemy dies

--- Assets/Scripts/EnemyBargesIn.cs
+++ Assets/Scripts/EnemyBargesIn.cs
@@ -12,10 +12,13 @@ public class EnemyBargesIn : MonoBehaviour
 
     private bool moving = false;
     private bool openDoor = false;
+    private bool closeDoor = false;
     private Quaternion targetRotation;
+    private Quaternion targetCloseRotation;
 
     void Start()
     {
+        targetCloseRotation = door.transform.rotation;
         StartCoroutine(EnemyEntrance());
     }
 
@@ -37,7 +40,21 @@ public class EnemyBargesIn : MonoBehaviour
         {
             transform.position = Vector3.MoveTowards(transform.position, endPosition.position, moveSpeed * Time.deltaTime);
             if (transform.position == endPosition.position)
+            {
                 moving = false;
+                openDoor = false;
+                closeDoor = true;
+                StartCoroutine(DieAfterDelay());
+            }            
         }
+        if (closeDoor)
+        {
+            door.transform.rotation = Quaternion.Lerp(door.transform.rotation, targetCloseRotation, doorOpenSpeed * Time.deltaTime);
+        }
+    }
+    IEnumerator DieAfterDelay()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
     }
 }
