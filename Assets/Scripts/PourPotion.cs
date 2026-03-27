c4ed8ee6a51ec88d6b3f3fe3e830beabd9e8aa73
Jasmine Thind <jasminethind8@gmail.com>
2026-03-27 15:23:05 -0400

    Add stream at a certain tilt level
    currently only for potion 1, will need to duplicate for 2

--- Assets/Scripts/PourPotion.cs
+++ Assets/Scripts/PourPotion.cs
@@ -5,6 +5,7 @@ public class PourPotion : MonoBehaviour
     public Transform spout;
     public float maxTilt = 45f;
     private bool isSpilling = false;
+    public ParticleSystem pourParticles;
 
     void Update()
     {
@@ -23,12 +24,29 @@ public class PourPotion : MonoBehaviour
     void StartPour()
     {
         isSpilling = true;
+
+        Transform fluidTransform = transform.Find("Fluid");
+
+        if (fluidTransform != null)
+        {
+
+            Color potionColor = fluidTransform.GetComponentInChildren<MeshRenderer>().material.GetColor("_Tint");
+
+            ParticleSystem.MainModule main = pourParticles.main;
+            main.startColor = potionColor;
+
+            ParticleSystem.TrailModule trails = pourParticles.trails;
+            trails.colorOverTrail = new ParticleSystem.MinMaxGradient(potionColor);
+        }
+
+        pourParticles.Play();
         Debug.Log("start pour");
     }
 
     void StopPour()
     {
         isSpilling = false;
+        pourParticles.Stop();
         Debug.Log("end pour");
     }
 }
