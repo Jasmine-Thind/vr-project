17b5503b543afbee03e90174f2012f7ae122dc78
Jasmine Thind <jasminethind8@gmail.com>
2026-03-27 16:52:22 -0400

    add script for collision

--- Assets/Scripts/PotionCollision.cs
+++ Assets/Scripts/PotionCollision.cs
@@ -4,4 +4,18 @@ public class PotionCollision : MonoBehaviour
 {
     public Liquid liquidScript;
     public Color mixColor = new Color(1f, 0.5f, 0f);
+    public float fillSpeed = 0.1f;
+    public float colorMixSpeed = 0.5f;
+
+    private void OnParticleCollision(GameObject other)
+    {
+        liquidScript.fillAmount -= fillSpeed * Time.deltaTime;
+        liquidScript.fillAmount = Mathf.Clamp(liquidScript.fillAmount, 0f, 1f);
+
+        Color currentColor = liquidScript.GetComponent<MeshRenderer>().material.GetColor("_TopColor");
+        Color newColor = Color.Lerp(currentColor, mixColor, colorMixSpeed * Time.deltaTime);
+
+        liquidScript.GetComponent<MeshRenderer>().material.SetColor("_TopColor", newColor);
+        liquidScript.GetComponent<MeshRenderer>().material.SetColor("_Tint", newColor);
+    }
 }
